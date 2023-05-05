be7d95910a42e54f88f5b0a3b9966428c81226c1
Rifqi Pratama <rifqipratama9@gmail.com>
2023-05-05 13:05:35 +0700

    add dotenv library

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -1,12 +1,13 @@
+import "dotenv/config";
 import { DataSource } from "typeorm";
 
 const myDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: "postgres",
-  password: "123456",
-  database: 'Botaniture',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ["src/entities/**/*.ts"],
   migrations: ["src/migrations/**/*.ts"],
   logging: true,
